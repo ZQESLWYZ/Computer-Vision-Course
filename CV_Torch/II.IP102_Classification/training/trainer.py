bc871f63c049167b2f81ab0d53c5a8c402568e02
ZQESLWYZ <zqeslwyz@163.com>
2025-11-25 13:31:26 +0800

    20251125_add_trainer

--- CV_Torch/II.IP102_Classification/training/trainer.py
+++ CV_Torch/II.IP102_Classification/training/trainer.py
@@ -2,6 +2,8 @@ import sys
 import os
 import torch
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+import datetime
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.config import read_yaml
 from models.resnet import ResNet34
@@ -9,10 +11,11 @@ from data.dataloader import get_dataloader
 from losses import get_criterion
 from optimizers import get_optimizer
 from schedulers import get_scheduler
+from scripts.evaluate import evaluate
 
 class trainer():
     """The class of model train"""
-    def __init__(self, config, model_name='resnet34'):
+    def __init__(self, config):
         
         self.config = config
         self.device = config['train']['device']
@@ -30,11 +33,14 @@ class trainer():
         self.exp_dir_name = config['exp']['save_dir']
         self.log_interval = config['exp']['log_interval']
         
+        log_dir = os.path.join(self.exp_dir_name, 'log', datetime.datetime.now().strftime(r"%Y%m%d-%H%M%S"))
+        self.writer = SummaryWriter(log_dir)
+        
     def start_train(self):
         if self.model_name == 'resnet34':
             model = ResNet34(self.num_class).to(self.device)
         # TODO: ADD MORE MODELS
-        else:
+        elif self.model_name == 'googlenet':
             pass
         
         train_loader, val_loader = get_dataloader(self.config)
@@ -44,20 +50,22 @@ class trainer():
             
         for epoch in range(self.epoch):
             model.train()
-            running_loss = 0.0
-            correct = 0
-            total = 0
-            
+
             for batch_idx, (inputs, targets) in enumerate(train_loader):
                 inputs = inputs.to(self.device)
                 targets = targets.to(self.device)
-                optimizer.zero_grad()
-                output = model(inputs)
                 
-                loss = criterion(output, targets)
+                # 梯度置零
+                optimizer.zero_grad()
+                
+                # forward + backward + optimize
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
                 loss.backward()
                 optimizer.step()
-            print(loss)
+                evaluate(epoch, model, val_loader, criterion, self.device, 'val', 5, self.writer)
+
+        print("Training Finish!")
 
 if __name__ == "__main__":
     config = read_yaml(r"CV_Torch\II.IP102_Classification\configs\train_config.yaml")
